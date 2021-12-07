29142dfdb3936e19fd3757158feaf12bb52bda28
lunatiq-san <lunatiq86@gmail.com>
2021-12-07 20:31:24 +0200

    Clear code

--- src/components/Variable/Variable.jsx
+++ src/components/Variable/Variable.jsx
@@ -5,10 +5,12 @@ import { toast } from "react-toastify";
 import ReactHtmlParser from "react-html-parser";
 
 import styles from "./Variable.module.css";
+import Loader from "react-loader-spinner";
 
 export default function Variable() {
   const { variableId } = useParams();
   const [variable, setVariable] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   const { DataType, Description, GroupName, ID, Name } = variable;
 
   useEffect(() => {
@@ -22,9 +24,11 @@ export default function Variable() {
 
         if (variableInfo === undefined) {
           toast.error("No such id exists");
+          setIsLoading(false);
+          return;
         }
-
         setVariable(variableInfo);
+        setIsLoading(false);
       })
       .catch((error) => console.log(error));
   }, [variableId]);
@@ -34,30 +38,41 @@ export default function Variable() {
       {variable !== undefined ? (
         <section className={styles.variable}>
           <h2 className={styles.title}>Variable</h2>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <span className={styles.itemKey}>DataType:</span>
-              <span className={styles.itemValue}>{DataType}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemKey}>Description:</span>
-              <span className={styles.description}>
-                {ReactHtmlParser(Description)}
-              </span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemKey}>GroupName:</span>
-              <span className={styles.itemValue}>{GroupName}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemKey}>ID:</span>
-              <span className={styles.itemValue}>{ID}</span>
-            </li>
-            <li className={styles.item}>
-              <span className={styles.itemKey}>Name:</span>
-              <span className={styles.itemValue}>{Name}</span>
-            </li>
-          </ul>
+
+          {isLoading ? (
+            <Loader
+              type="Puff"
+              color="#00adb5"
+              height={50}
+              width={50}
+              className={styles.loader}
+            />
+          ) : (
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <span className={styles.itemKey}>DataType:</span>
+                <span className={styles.itemValue}>{DataType}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemKey}>Description:</span>
+                <span className={styles.description}>
+                  {ReactHtmlParser(Description)}
+                </span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemKey}>GroupName:</span>
+                <span className={styles.itemValue}>{GroupName}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemKey}>ID:</span>
+                <span className={styles.itemValue}>{ID}</span>
+              </li>
+              <li className={styles.item}>
+                <span className={styles.itemKey}>Name:</span>
+                <span className={styles.itemValue}>{Name}</span>
+              </li>
+            </ul>
+          )}
         </section>
       ) : (
         <p>Variable with ID: {variableId} not found</p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,10 +6,12 @@ import Searchbar from "../components/Searchbar";
 import ListMostRecentDecoded from "../components/ListMostRecentDecoded";
 import { carsSelectors } from "../redux/cars";
 import styles from "./HomePage.module.css";
+import Loader from "react-loader-spinner";
 
 export default function HomePage() {
   const [lastRequests, setLastRequests] = useState([]);
   const carInfo = useSelector(carsSelectors.getCarInfo);
+  const isLoading = useSelector(carsSelectors.isLoading);
 
   return (
     <main className="HomePage">
@@ -18,6 +20,15 @@ export default function HomePage() {
         <Searchbar />
         <ListMostRecentDecoded />
       </RequestContext.Provider>
+      {isLoading && (
+        <Loader
+          type="Puff"
+          color="#00adb5"
+          height={50}
+          width={50}
+          className={styles.loader}
+        />
+      )}
       {carInfo ? <DecodeList /> : null}
     </main>
   );
